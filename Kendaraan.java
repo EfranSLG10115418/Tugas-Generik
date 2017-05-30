876b5e1cf87bb1e4e507fdfa812f3f6aaaa1760b
Efran Sucipto Lumban Gaol <efransuciptolumbangaol@gmail.com>
2017-05-30 23:33:59 +0700

    Add files via upload

---
+++ Kendaraan.java
@@ -0,0 +1,11 @@
+public class Kendaraan <T> {
+		private T Mobil;
+	 
+	public T getMobil(){
+		return Mobil;
+	}
+
+	p ublic void setMobil(T Mobil){
+		this.Mobil=Mobil;
+	}
+}
---
+++ main.java
@@ -0,0 +1,7 @@
+public class main{
+	public static void main (String[]args){
+	Kendaraan<String> MobilString = new Kendaraan <>();
+	MobilString.setMobil("BMW");
+	System.out.println(MobilString.getMobil());
+	}
+}
